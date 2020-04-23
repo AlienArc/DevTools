c781ef834f82b29aa07d48b582470c73bae62a3a
Duane Newman <duane@lisanduane.net>
2020-04-23 15:36:49 -0500

    Added command Update-EntityFrameworkDatabase

--- PowerShell/EntityFramework.psm1
+++ PowerShell/EntityFramework.psm1
@@ -9,6 +9,44 @@ function ExportFunctionWithAlias
     Export-ModuleMember -Alias $name
 }
 
+function OpenLocalDb()
+{
+    $connection = New-Object System.Data.SqlClient.SqlConnection
+    $connection.ConnectionString = "Server=(localdb)\MSSQLLocalDB;Database=Master;Trusted_Connection=True;"
+    $connection.Open()
+    return $connection
+}
+
+function DropDatabase ([System.Data.SqlClient.SqlConnection]$Connection, [string]$DatabaseName, [string]$ProjectPath)
+{    
+    $singleUserModeCommand = New-Object System.Data.SqlClient.SqlCommand
+    $singleUserModeCommand.CommandText = "alter database [$DatabaseName] set single_user with rollback immediate"
+    $singleUserModeCommand.Connection = $Connection
+    Write-Output "Setting database [$DatabaseName] to single user mode..."
+    $result = $singleUserModeCommand.ExecuteNonQuery()
+
+    $dropCommand = New-Object System.Data.SqlClient.SqlCommand
+    $dropCommand.CommandText = "drop database [$DatabaseName]"
+    $dropCommand.Connection = $Connection
+    Write-Output "Dropping database [$DatabaseName]..."
+    $result = $dropCommand.ExecuteNonQuery()
+}
+
+function EnsureDatabase ([System.Data.SqlClient.SqlConnection]$Connection, [string]$DatabaseName,[string]$ProjectPath)
+{    
+    $createCommand = New-Object System.Data.SqlClient.SqlCommand
+    $createCommand.CommandText = "IF NOT EXISTS(SELECT * FROM sys.sysdatabases where name='$DatabaseName') create database [$DatabaseName]"
+    $createCommand.Connection = $Connection
+    Write-Output "Ensuring database [$DatabaseName] exists..."
+    $result = $createCommand.ExecuteNonQuery()
+}
+
+function RunMigrationUpdate ([string]$ProjectPath)
+{
+    Write-Output "Running EF database migrations..."
+    dotnet ef database update --project $ProjectPath 
+}
+
 function Reset-EntityFrameworkDatabase
 {
 	Param(
@@ -16,32 +54,30 @@ function Reset-EntityFrameworkDatabase
         [Parameter(Mandatory=$True,Position=2)] [string]$ProjectPath
     )
 
-    $connection = New-Object System.Data.SqlClient.SqlConnection
-    $connection.ConnectionString = "Server=(localdb)\MSSQLLocalDB;Database=Master;Trusted_Connection=True;"
-    $connection.Open()
+    $connection = OpenLocalDb
 
-    $singleUserModeCommand = New-Object System.Data.SqlClient.SqlCommand
-    $singleUserModeCommand.CommandText = "alter database [$DatabaseName] set single_user with rollback immediate"
-    $singleUserModeCommand.Connection = $connection
-    Write-Output "Setting database [$DatabaseName] to single user mode..."
-    $result = $singleUserModeCommand.ExecuteNonQuery()
+    DropDatabase $connection $DatabaseName $ProjectPath
+    EnsureDatabase $connection $DatabaseName $ProjectPath
+    RunMigrationUpdate $ProjectPath
 
-    $dropCommand = New-Object System.Data.SqlClient.SqlCommand
-    $dropCommand.CommandText = "drop database [$DatabaseName]"
-    $dropCommand.Connection = $connection
-    Write-Output "Dropping database [$DatabaseName]..."
-    $result = $dropCommand.ExecuteNonQuery()
+    $connection.Close()
+    
+}
+ExportFunctionWithAlias 'EF-ResetDB' 'Reset-EntityFrameworkDatabase'
 
-    $createCommand = New-Object System.Data.SqlClient.SqlCommand
-    $createCommand.CommandText = "create database [$DatabaseName]"
-    $createCommand.Connection = $connection
-    Write-Output "Creating database [$DatabaseName]..."
-    $result = $createCommand.ExecuteNonQuery()
+function Update-EntityFrameworkDatabase
+{
+	Param(
+        [Parameter(Mandatory=$True,Position=1)] [string]$DatabaseName,
+        [Parameter(Mandatory=$True,Position=2)] [string]$ProjectPath
+    )
+
+    $connection = OpenLocalDb
+
+    EnsureDatabase $connection $DatabaseName $ProjectPath
+    RunMigrationUpdate $ProjectPath
 
     $connection.Close()
 
-    Write-Output "Running EF database migrations..."
-    dotnet ef database update --project $ProjectPath 
 }
-
-ExportFunctionWithAlias 'EF-ResetDB' 'Reset-EntityFrameworkDatabase'
+ExportFunctionWithAlias 'EF-UpdateDB' 'Update-EntityFrameworkDatabase'
