2ae7c814683a56b44e1d39f5109f97d597984a9e
Duane Newman <duane@alienarc.com>
2020-08-11 15:41:38 -0500

    renamed Update-AllGitRepos to Update-GitRepos. Added filter parameter to limit repos. Added path parameter to allow running from another directory

--- PowerShell/AlienArcExtensions/Modules/GitExtensions/GitExtensions.psm1
+++ PowerShell/AlienArcExtensions/Modules/GitExtensions/GitExtensions.psm1
@@ -1,39 +1,69 @@
-function Update-AllGitRepos {
+function Update-GitRepos {
+    param (
+        [Parameter(Mandatory=$False)]
+        [string[]]
+        $Filter,
+        [Parameter(Mandatory=$False)]
+        [string]
+        $Path
+    )
 
-    Push-Location
+    begin {
+        Push-Location
+    }
+    
+    process {
+        if ($Path -ne $null -and $Path -ne "")
+        {
+            set-location $Path
+        }
 
-    Get-ChildItem -Recurse -Depth 2 -Force | 
-        Where-Object { $_.Mode -match "h" -and $_.FullName -like "*\.git" } |
-            ForEach-Object {
-                Set-Location "$($_.FullName)/.."
-                $repo = (Get-Item .).Name
-                $curBranch = git branch --show-current
-                if ($curBranch -ne "develop")
-                {
-                    Write-Host "$repo - skipping, not on develop branch" -ForegroundColor "Yellow"
-                    continue
+        Get-ChildItem -Recurse -Depth 2 -Force | 
+            Where-Object { $_.Mode -match "h" -and $_.FullName -like "*\.git" } |
+                ForEach-Object {
+                    
+                    if ($filter -ne $null -and (($filter -contains (Get-Item "$($_.FullName)/..").Name) -eq $false)) 
+                    {
+                        return
+                    }
+
+                    Set-Location "$($_.FullName)/.."
+                    $repo = (Get-Item .).Name
+                    $curBranch = git branch --show-current
+
+                    Write-Host "$repo [$curBranch] - " -NoNewline -ForegroundColor "White"
+
+                    if ($curBranch -ne "develop" -and $curBranch -ne "master")
+                    {
+                        Write-Host "skipping, not on develop or master branch" -ForegroundColor "Red" -BackgroundColor "Black"
+                        return
+                    }
+                
+                    #Write-Host "Updating" $repo [$curBranch]
+
+                    #Write-Host "$repo - Updating remotes & pruning branches"
+                    git remote update --prune
+
+                    $localRev  = (git rev-parse HEAD)
+                    $remoteRev = (git rev-parse "@{u}")
+                    if ($localRev -eq $remoteRev)
+                    {
+                        Write-Host "Allready up to date" -ForegroundColor "Green"
+                    }
+                    else
+                    {
+                        Write-Host "Pulling latest" -ForegroundColor "Yellow"
+                        git pull
+                        #Write-Host "- Pulled latest" -ForegroundColor "Yellow"
+                    }
                 }
-            
-                Write-Host "Updating" $repo [$curBranch]
 
-                #Write-Host "$repo - Updating remotes & pruning branches"
-                git remote update --prune
-
-                $localRev  = (git rev-parse HEAD)
-                $remoteRev = (git rev-parse "@{u}")
-                if ($localRev -eq $remoteRev)
-                {
-                    Write-Host "$repo - Allready up to date"
-                }
-                else
-                {
-                    git pull
-                    Write-Host "$repo - Pulled latest"
-                }
-            }
-
-            
-    Pop-Location
+    }
+    
+    end {        
+        Pop-Location
+    }
 
 }
-Set-Alias Git-UpdateRepos Update-AllGitRepos
+
+Set-Alias Git-UpdateRepos Update-GitRepos
